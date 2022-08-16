797af2f745e1ae798ce0b02fca2399f1d0170f39
Praise Adegoke <adegokepraise6@gmail.com>
2022-08-15 20:48:41 -0700

    second commit

--- talent.js
+++ talent.js
@@ -1,16 +1,24 @@
 const modal = document.querySelector('.modal');
+const modal2 = document.querySelector('.modal-2');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnsOpenModal2 = document.querySelectorAll('.btn--show-modal-2');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 
 const openModal = function (e) {
     e.preventDefault()
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-  };
+};
+const openModal2 = function (e) {
+    e.preventDefault()
+    modal2.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
   
 const closeModal = function () {
     modal.classList.add('hidden');
+    modal2.classList.add('hidden');
     overlay.classList.add('hidden');
 };
   btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
@@ -22,3 +30,5 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+btnsOpenModal2.forEach(btn => btn.addEventListener('click', openModal2))
